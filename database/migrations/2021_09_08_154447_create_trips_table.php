e9c025f501a1e830bffea224f945329506abed1d
La-wii <sentilavale@gmail.com>
2021-09-08 18:18:23 +0200

    first commit

---
+++ database/migrations/2021_09_08_154447_create_trips_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',80);
+            $table->string('city',80);
+            $table->string('description',300);
+            $table->float('price', 6, 2)->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.app')
+@section('title', 'titolo pagina')
+@section('content')
+<h1>Ciao</h1>
+@endsection
